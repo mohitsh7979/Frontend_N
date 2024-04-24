187d22f91510ed6876219189b3b8eb622ea4aee6
mohitsh7979 <mohitsh7979@gmail.com>
2024-04-24 12:34:03 +0530

    Update

---
+++ js/variable.js
@@ -0,0 +1,46 @@
+// var a = 10;
+// console.log(a)
+
+// let b = 50;
+// b = 60;
+// console.log(b)
+
+// const c = 40;
+// c = 50;
+// console.log(c)
+
+
+{
+    var a = 10; // global variable
+    let b = 20; // local variable  2015 ES6
+    const c = 50; // local variable 2015 ES6
+    d = 10  // var default
+}
+// console.log(c)
+// console.log(b)
+// console.log(a)
+// console.log(d)
+
+
+// var a = 10;
+
+// for(let a = 0; a<5;a++){
+//   console.log(a)
+// }
+
+// console.log(a)
+
+// Hosting variable
+// console.log(x=20)
+
+// var x; hosting variable
+
+// console.log(y=30)
+
+// let y;
+
+// console.log(y)
+
+// const y;
+
+
